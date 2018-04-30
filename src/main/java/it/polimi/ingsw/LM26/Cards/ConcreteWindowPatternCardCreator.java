00dbaf848816a5464736f86a2ea719a72299a3ab
eugenio cortesi <eugeniocortesi@yahoo.com>
2018-04-30 11:45:30 +0200

    serialization and cards implementation

--- src/main/java/it/polimi/ingsw/LM26/Cards/ConcreteWindowPatternCardCreator.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.LM26.Cards;
-
-public class ConcreteWindowPatternCardCreator extends CardCreatorAC{
-
-    public static CardInt createCard(){
-        return new WindowPatternCard();
-    }
-}
