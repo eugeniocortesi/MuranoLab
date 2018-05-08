2288092b043892715ce57368b78e7dfc9109d7d0
Chiara Criscuolo <10541002@polimi.it>
2018-05-08 11:54:15 +0200

    " Added first test on Bag and Die"

---
+++ src/test/java/it/polimi/ingsw/LM26/PlayArea/TestBagSimple.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.LM26.PlayArea;
+import it.polimi.ingsw.LM26.Cards.Color;
+import it.polimi.ingsw.LM26.PlayArea.Bag;
+import org.junit.Test;
+
+public class TestBagSimple {
+
+    Bag bag = new Bag();
+
+    @Test
+    public void checkBag(){
+
+        bag.dump();
+        System.out.println("--------");
+        for (int i = 0; i <  90; i++) {
+            Die d = bag.Draw();
+            d.roll();
+            System.out.println("d: " + d);
+            //d.dump();
+            bag.dump();
+        }
+    }
+
+}
